95a7e7c1d680648a7e4bb93399f4c0d92307639d
brittneypostma <bdesignedwebdev@gmail.com>
2022-07-21 12:56:44 -0400

    init

---
+++ svelte.config.js
@@ -0,0 +1,17 @@
+import adapter from '@sveltejs/adapter-netlify'
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: adapter({
+      edge: true
+    }),
+
+    // Override http methods in the Todo forms
+    methodOverride: {
+      allowed: ['PATCH', 'DELETE']
+    }
+  }
+}
+
+export default config
